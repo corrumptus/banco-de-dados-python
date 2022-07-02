38dde3985202e578c032ac34e4ad31a2d81e50c5
corrumptus <lazarinilucas42@gmail.com>
2022-07-02 15:26:19 -0300

    progress3

--- db.py
+++ db.py
@@ -40,24 +40,27 @@ def Contatos():
 	return
 
 def Logar_usuario():
-
-	return
+	nome = input('Digite o seu nome de usuário\n').strip()
+	senha = input('Digite a sua senha\n').strip()
+	if Checa_senha(nome,senha) == True:
+		return True
+	return False
 
 def Cadastra_conta():
-	nome = input('Digite o seu nome de usuários\n').lower()
+	nome = input('Digite o seu nome de usuários\n').lower().strip()
 	if Checa_usu(nome) == False:
-		senha = input('Digite a sua senha(não se esqueça dela)\n').lower()
+		senha = input('Digite a sua senha(não se esqueça dela)\n').lower().strip()
 		if len(senha) >= 6:
-			senha2 = input('Digite a sua senha novamente(não se esqueça dela)\n').lower()
+			senha2 = input('Digite a sua senha novamente(não se esqueça dela)\n').lower().strip()
 			if senha == senha2:
-				arq = open(nome+','+senha+'.txt', 'w', encoding='utf8')
+				arq = open(nome + ',' + senha + '.txt', 'w', encoding='utf8')
 				arq.write('-------Avisos/Lembretes-------\n')
 				arq.write('------------Texto-------------\n')
 				arq.write('---------Área secreta---------\n')
 				arq.close()
-				tipo = input('Digite um tipo de contato para segurança(email, telefone...)\n')
-				contato = input('Digite um contato de segurança\n')
-				Adiciona_nome(nome+','+senha+'.txt', contato, tipo)
+				tipo = input('Digite um tipo de contato para segurança(email, telefone...)\n').strip()
+				contato = input('Digite um contato de segurança\n').strip()
+				Adiciona_nome(nome + ',' + senha + '.txt', contato, tipo)
 			else:
 				print('Senhas diferentes, tente novamente')
 		else:
@@ -67,7 +70,7 @@ def Cadastra_conta():
 	return
 
 def Carrega_ADM():
-	arq = open(arq_adm, 'r')
+	arq = open('AdminAdmin.txt', 'r')
 	arquivo = []
 	for i in arq:
 		arquivo.append(i.replace('\n',''))
@@ -76,6 +79,7 @@ def Carrega_ADM():
 			n = i
 		if arquivo[i] == '-------Log-------':
 			n2 = i
+	arq.close()
 	return arquivo, n, n2
 
 def Checa_usu(nome):
@@ -84,45 +88,65 @@ def Checa_usu(nome):
 		n3 = arq_ADM[i].find(',')
 		if arq_ADM[i][0:n3] == nome:
 			return True
-	return False 
+	return False
+
+def Checa_senha(nome, senha):
+	arq_ADM, n, n2 = Carrega_ADM()
+	for i in range(n+1, n2):
+		n3 = arq_ADM[i].find(',')
+		n4 = arq_ADM[i].find('.txt')
+		if arq_ADM[i][0:n3] == nome and arq_ADM[i][n3+1:n4] == senha:
+			return 
 
 def Suporte():
-	usuario = input('Digite o seu nome de usuário\n') #checar se existe
-	contato = input('Digite o seu contato cadastrado\n') #checar validade
+	usuario = input('Digite o seu nome de usuário\n').strip() #checar se existe
+	contato = input('Digite o seu contato cadastrado\n').strip() #checar validade
 	a_ADM, n, n2 = Carrega_ADM()
-	arq_ADM = open(arq_adm, 'w')
+	arq_ADM = open('AdminAdmin.txt', 'w')
 	for i in range(n):
 		arq_ADM.write(a_ADM[i])
-	arq_ADM.write('o usuário ' + usuario + 'perdeu sua senha. Entrar em contato com o mesmo via: ' + contato + '(' + tipo + ')') #achar o tipo dentro do arquivo admin
+	arq_ADM.write('o usuário ' + usuario + 'perdeu sua senha. Entrar em contato com o mesmo via: ' + contato + '(' + Acha_tipo(usuario) + ')')
 	for i in range(n, len(a_ADM)+1):
 		arq_ADM.write(a_ADM[i])
+	arq_ADM.write('O usuario ' + usuario + ' pediu ajuda ao suporte')
+	arq_ADM.close()
 	return
 
 def Adiciona_nome(nome, contato, tipo):
 	arq_ADM, n, n2= Carrega_ADM()
-	arq = open(arq_adm, 'w', encoding='utf8')
+	arq = open('AdminAdmin.txt', 'w', encoding='utf8')
 	for i in range(n):
 		arq.write(arq_ADM[i]+'\n')
 	for i in range(n, n2):
 		arq.write(arq_ADM[i]+'\n')
-	arq.write(nome + ' || ' + contato + ' || ' + tipo+'\n')
+	arq.write(nome + ' || ' + contato + ' || ' + tipo + '\n')
 	for i in range(n2, len(arq_ADM)):
 		arq.write(arq_ADM[i]+'\n')
-	arq.write('Novo usuário criado: '+ nome+'\n')
+	arq.write('Novo usuário criado: ' + nome + '\n')
 	arq.close()
-	log = open('Logs.txt','a', encoding='utf8')
-	log.write('Novo usuário criado: '+ nome+'\n')
+	log = open('Logs.txt', 'a', encoding='utf8')
+	log.write('Novo usuário criado: ' + nome + '\n')
 	log.close()
 
+def Acha_tipo(nome):
+	arq_ADM, n, n2 = Carrega_ADM()
+	for i in range(n+1,n2):
+		n3 = arq_ADM[i].find(',')
+		if arq_ADM[i][0:n3] == nome:
+			for j in range(len(arq_ADM[i])-1, 0, -1):
+				if arq_ADM[i][j] == ' ':
+					return arq_ADM[i][j+1:]
 
-arq_adm = 'AdminAdmin.txt'
 opcao = 1
 while opcao != 0: #criara função main
 	opcao = menu_nCad()
 	if opcao == 1:
-		op = 1
-		while op != 0:
-			op = menu_Cad()
+		if Logar_usuario() == True:
+			op = 1
+			while op != 0:
+				op = menu_Cad()
+		else:
+			print('Usuario e/ou senha incorreto(s), tente novamente')
 	elif opcao == 2:
 		op = 1
 		while op != 0:
