7358332d997f2681fdebdb46dea2510faf9b3e49
corrumptus <lazarinilucas42@gmail.com>
2022-07-03 00:18:31 -0300

    progress4

--- db.py
+++ db.py
@@ -1,28 +1,5 @@
 import os
 
-def menu_nCad():
-	print('1 - Entrar em uma conta')
-	print('2 - Entrar como administrador')
-	print('3 - Contatos')
-	print('4 - Cadastrar uma conta')
-	print('5 - Suporte')
-	print('0 - Fechar o programa')
-	opcao = int(input('Digite a opção desejada\n'))
-	return opcao
-
-def menu_Cad():
-	print('1 - Ver todo o arquivo')
-	print('2 - Ver só os avisos/lembretes')
-	print('3 - Ver só o texto')
-	print('4 - Editar avisos')
-	print('5 - Editar texto')
-	print('6 - Entrar na área secreta')
-	print('7 - Trocar nome de usuário/senha')
-	print('8 - Apagar conta')
-	print('9 - Voltar ao menu principal')
-	opcao = int(input('Digite a opção desejada\n'))
-	return opcao
-
 def menu_Adm():
 	print('1 - Ver quantas pessoas estão cadastradas')
 	print('2 - Listar pessoas cadastradas')
@@ -38,13 +15,6 @@ def Contatos():
 	print('email: surx42@gmail.com')
 	print('telefone: (12) 93456-7810')
 	return
-
-def Logar_usuario():
-	nome = input('Digite o seu nome de usuário\n').strip()
-	senha = input('Digite a sua senha\n').strip()
-	if Checa_senha(nome,senha) == True:
-		return True
-	return False
 
 def Cadastra_conta():
 	nome = input('Digite o seu nome de usuários\n').lower().strip()
@@ -96,7 +66,8 @@ def Checa_senha(nome, senha):
 		n3 = arq_ADM[i].find(',')
 		n4 = arq_ADM[i].find('.txt')
 		if arq_ADM[i][0:n3] == nome and arq_ADM[i][n3+1:n4] == senha:
-			return 
+			return True
+	return False
 
 def Suporte():
 	usuario = input('Digite o seu nome de usuário\n').strip() #checar se existe
@@ -137,23 +108,77 @@ def Acha_tipo(nome):
 				if arq_ADM[i][j] == ' ':
 					return arq_ADM[i][j+1:]
 
-opcao = 1
-while opcao != 0: #criara função main
-	opcao = menu_nCad()
-	if opcao == 1:
-		if Logar_usuario() == True:
+def Exibir(nome, sec1, sec2, sec3):
+	arq = open(nome, 'r')
+	arquivo = []
+	for i in arq:
+		arquivo.append(i.replace('\n',''))
+	for i in range(len(arquivo)):
+		if arquivo[i] == '------------Texto-------------':
+			n = i
+		if arquivo[i] == '---------Área secreta---------':
+			n2 = i
+	for i in range(0*sec1, n*sec1):
+		print(arquivo[i])
+	for i in range(n*sec2, n2*sec2):
+		print(arquivo[i])
+	for i in range(n2*sec3, len(arquivo)*sec3):
+		print(arquivo[i])
+	return
+
+def main():
+	opcao = 1
+	while opcao != 0:
+		print('1 - Entrar em uma conta')
+		print('2 - Entrar como administrador')
+		print('3 - Contatos')
+		print('4 - Cadastrar uma conta')
+		print('5 - Suporte')
+		print('0 - Fechar o programa')
+		opcao = int(input('Digite a opção desejada\n'))
+		if opcao == 1:
+			main_logar()
+		elif opcao == 2:
 			op = 1
 			while op != 0:
-				op = menu_Cad()
-		else:
-			print('Usuario e/ou senha incorreto(s), tente novamente')
-	elif opcao == 2:
-		op = 1
-		while op != 0:
-			op = menu_Adm()
-	elif opcao == 3:
-		Contatos()
-	elif opcao == 4:
-		Cadastra_conta()
-	elif opcao == 5:
-		Suporte()
+				op = menu_Adm()
+		elif opcao == 3:
+			Contatos()
+		elif opcao == 4:
+			Cadastra_conta()
+		elif opcao == 5:
+			Suporte()
+	return
+
+def main_logar():
+	nome = input('Digite o seu nome de usuário\n').strip().lower()
+	senha = input('Digite a sua senha\n').strip().lower()
+	if Checa_senha(nome, senha) == True:
+		opcao = 1
+		arquivo = nome + ',' + senha + '.txt'
+		while opcao != 0:
+			print('1 - Ver todo o arquivo')
+			print('2 - Ver só os avisos/lembretes')
+			print('3 - Ver só o texto')
+			print('4 - Editar avisos') ##
+			print('5 - Editar texto') ##
+			print('6 - Entrar na área secreta')
+			print('7 - Solicitar suporte') ##
+			print('8 - Trocar nome de usuário/senha')
+			print('9 - Apagar conta') 
+			print('10 - Ver tutorial') ##
+			print('11 - Voltar ao menu principal')
+			opcao = int(input('Digite a opção desejada\n'))
+			if opcao == 1:
+				Exibir(arquivo, 1, 1, 0)
+			elif opcao == 2:
+				Exibir(arquivo, 1, 0, 0)
+			elif opcao == 3:
+				Exibir(arquivo, 0, 1, 0)
+			elif opcao == 4:
+				Editar()
+	else:
+		print('Usuario e/ou senha incorreto(s), tente novamente')
+	return
+
+main()
